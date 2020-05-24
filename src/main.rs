54b9ec33aafcbf60a82667e7c62df1e725bf7f95
Henry Luengas <gyatso@pop-os.localdomain>
2020-05-23 17:25:20 -0700

    intial commit 2

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+struct Color {
+    channel_1: f32,
+    channel_2: f32,
+    channel_3: f32,
+}
+
+fn main() {
+    generate_colors(8);
+}
+
+fn generate_colors(color_bit_depth: u8) -> Vec<Color> {
+    println!("Hello, world!");
+    
+    let color_list: Vec<Color> = Vec::new();
+    return color_list
+}
