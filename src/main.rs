8357d57a1bc35451aa8e5ee29936998284523427
Henry Luengas <henry@luengas.dev>
2022-10-01 11:28:19 -0500

    completed implementation of new struct model, added visiualization for the boundry region

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use image::{ImageFormat, Rgb, RgbImage};
+use image::{GrayImage, ImageFormat, Luma, Rgb, RgbImage};
 use rand::random;
+use std::time::Instant;
 
 struct Painting {
     image: RgbImage,
-    boundry_region: Vec<Coordinate>,
+    boundry_region_image: GrayImage,
+    boundry_region_list: Vec<Coordinate>,
     starting_locations: Vec<Coordinate>,
     canvas_constraints: Constraints,
     canvas_stats: Stats,
@@ -13,14 +15,15 @@ struct Constraints {
     y_size: u32,
 }
 struct Stats {
-    total_pixel_count: u64,
     current_pixels_placed_count: u64,
-    previous_pixels_placed_count: u64,
+    // previous_pixels_placed_count: u64,
+    // total_pixel_count: u64,
 }
 struct Pixel {
     position: Coordinate,
     color: Rgb<u8>,
 }
+#[derive(Copy, Clone)]
 struct Coordinate {
     x: u32,
     y: u32,
@@ -28,114 +31,152 @@ struct Coordinate {
 
 fn main() {
     let working_constraints: Constraints = Constraints {
-        x_size: 128u32,
-        y_size: 128u32,
+        x_size: 256u32,
+        y_size: 256u32,
     };
-    let mut working_stats: Stats = Stats {
-        total_pixel_count: (128u64 * 128u64),
+    let working_stats: Stats = Stats {
+        // total_pixel_count: (working_constraints.x_size as u64 * working_constraints.y_size as u64),
         current_pixels_placed_count: 0u64,
-        previous_pixels_placed_count: 0u64,
+        // previous_pixels_placed_count: 0u64,
     };
+    let mut starting_points = Vec::new();
+    for i in 0..working_constraints.x_size {
+        if i % 64 == 0 {
+            starting_points.push(Coordinate { x: 0, y: i });
+            starting_points.push(Coordinate { x: working_constraints.x_size - 1, y: i });
+
+            starting_points.push(Coordinate { x: i, y: 0 });
+            starting_points.push(Coordinate { x: i, y: working_constraints.x_size - 1 });
+        }
+    }
     let mut working_canvas: Painting = Painting {
         image: RgbImage::new(working_constraints.x_size, working_constraints.y_size),
+        boundry_region_image: GrayImage::new(
+            working_constraints.x_size,
+            working_constraints.y_size,
+        ),
+        // starting_locations: vec![Coordinate {
+        //     x: working_constraints.x_size / 2u32,
+        //     y: working_constraints.y_size / 2u32,
+        // }],
+        starting_locations: starting_points,
         canvas_constraints: working_constraints,
         canvas_stats: working_stats,
-        boundry_region: Vec::new(),
-        starting_locations: vec![
-            Coordinate { x: 0, y: 0 },
-            Coordinate {
-                x: working_constraints.x_size - 1,
-                y: working_constraints.y_size - 1,
-            },
-        ],
+        boundry_region_list: Vec::new(),
     };
 
     for index in 0..working_canvas.starting_locations.len() {
-        place_pixel(&mut working_canvas)
+        let target_pixel = Pixel {
+            position: working_canvas.starting_locations[index].clone(),
+            color: Rgb([random::<u8>(), random::<u8>(), random::<u8>()]),
+        };
+        place_pixel(&target_pixel, &mut working_canvas)
     }
 
-    canvas
+    working_canvas
+        .image
         .save_with_format("./output/painting.png", ImageFormat::Png)
         .unwrap();
 
-    while boundry_region.len() > 0 {
+    working_canvas
+        .boundry_region_image
+        .save_with_format("./output/boundry.png", ImageFormat::Png)
+        .unwrap();
+
+    let mut current_time = Instant::now();
+    while working_canvas.boundry_region_list.len() > 0 {
         let target_color = Rgb([random::<u8>(), random::<u8>(), random::<u8>()]);
-        let best_position = get_best_position_for_color();
-        place_pixel();
-        if current_pixels_placed_count % 50 == 0 {
-            canvas
+        let target_pixel = get_best_position_for_color(target_color, &mut working_canvas);
+
+        place_pixel(&target_pixel, &mut working_canvas);
+
+        if current_time.elapsed().as_secs_f32() > (1f32 / 2f32) {
+            current_time = Instant::now();
+            working_canvas
+                .image
                 .save_with_format("./output/painting.png", ImageFormat::Png)
+                .unwrap();
+
+            working_canvas
+                .boundry_region_image
+                .save_with_format("./output/boundry.png", ImageFormat::Png)
                 .unwrap();
         }
     }
-    canvas
+    working_canvas
+        .image
         .save_with_format("./output/painting.png", ImageFormat::Png)
+        .unwrap();
+
+    working_canvas
+        .boundry_region_image
+        .save_with_format("./output/boundry.png", ImageFormat::Png)
         .unwrap();
 }
 
-fn place_pixel(target: Pixel, working_canvas: &mut Painting) {
+fn place_pixel(target: &Pixel, working_canvas: &mut Painting) {
+    working_canvas
+        .image
+        .put_pixel(target.position.x, target.position.y, target.color);
+    working_canvas.canvas_stats.current_pixels_placed_count += 1;
 
-    working_canvas.image.put_pixel(target.x, target.y, rgb_pixel);
-    *current_pixels_placed_count += 1;
     for i in 0..3 {
-        'inner: for j in 0..3 {
+        for j in 0..3 {
             // skip self
             if i == 1 && j == 1 {
                 continue;
             }
             // prevent less than zero out-of-bounds
-            if x_coord == 0 && i == 0 {
+            if target.position.x == 0 && i == 0 {
                 continue;
             }
-            if y_coord == 0 && j == 0 {
+            if target.position.y == 0 && j == 0 {
                 continue;
             }
             // prevent greater than dimensions out-of-bounds
-            if x_coord == (x_dim - 1) && i == 2 {
+            if target.position.x == (working_canvas.canvas_constraints.x_size - 1) && i == 2 {
                 continue;
             }
-            if y_coord == (y_dim - 1) && j == 2 {
+            if target.position.y == (working_canvas.canvas_constraints.y_size - 1) && j == 2 {
                 continue;
             }
 
-            let neighbor_x_coord: u32 = x_coord + i - 1;
-            let neighbor_y_coord: u32 = y_coord + j - 1;
+            let neighbor_x_coord: u32 = target.position.x + i - 1;
+            let neighbor_y_coord: u32 = target.position.y + j - 1;
 
-            for index in 0..boundry_region.len() {
-                if boundry_region[index] == (neighbor_x_coord, neighbor_y_coord) {
-                    continue 'inner;
-                }
+            let neighbor_luma: Luma<u8> = *working_canvas
+                .boundry_region_image
+                .get_pixel(neighbor_x_coord, neighbor_y_coord);
+            let neighbor_color: Rgb<u8> = *working_canvas
+                .image
+                .get_pixel(neighbor_x_coord, neighbor_y_coord);
+
+            // ensure locations are not added to the boundry region as duplicates
+            if neighbor_luma != Luma([0u8]) || neighbor_color != Rgb([0u8, 0u8, 0u8]) {
+                continue;
             }
 
-            let neighbor_color: Rgb<u8> = *canvas.get_pixel(neighbor_x_coord, neighbor_y_coord);
-
-            if neighbor_color == Rgb([0, 0, 0]) {
-                // println!(
-                //     "Adding {},{} from boundry region.",
-                //     neighbor_x_coord, neighbor_y_coord
-                // );
-                boundry_region.push((neighbor_x_coord, neighbor_y_coord));
-            }
+            working_canvas.boundry_region_list.push(Coordinate {
+                x: neighbor_x_coord,
+                y: neighbor_y_coord,
+            });
+            working_canvas.boundry_region_image.put_pixel(
+                neighbor_x_coord,
+                neighbor_y_coord,
+                Luma([255u8]),
+            );
         }
     }
 }
 
-fn get_best_position_for_color(
-    target_color: &Rgb<u8>,
-    canvas: &RgbImage,
-    x_dim: u32,
-    y_dim: u32,
-    boundry_region: &mut Vec<(u32, u32)>,
-) -> (u32, u32) {
+fn get_best_position_for_color(target_color: Rgb<u8>, working_canvas: &mut Painting) -> Pixel {
     let mut min_color_difference: f32 = f32::MAX;
     let mut best_position_index: usize = 0;
 
     // loop over every available position
-    for index in 0..boundry_region.len() {
+    for index in 0..working_canvas.boundry_region_list.len() {
         let mut color_difference_sum: f32 = 0f32;
         let mut neighbor_count: u64 = 0;
-        let x_coord: u32 = boundry_region[index].0;
-        let y_coord: u32 = boundry_region[index].1;
 
         // loop over neighbors
         for i in 0..3 {
@@ -145,26 +186,34 @@ fn get_best_position_for_color(
                     continue;
                 }
                 // prevent less than zero out-of-bounds
-                if x_coord == 0 && i == 0 {
+                if working_canvas.boundry_region_list[index].x == 0 && i == 0 {
                     continue;
                 }
-                if y_coord == 0 && j == 0 {
+                if working_canvas.boundry_region_list[index].y == 0 && j == 0 {
                     continue;
                 }
                 // prevent greater than dimensions out-of-bounds
-                if x_coord == (x_dim - 1) && i == 2 {
+                if working_canvas.boundry_region_list[index].x
+                    == (working_canvas.canvas_constraints.x_size - 1)
+                    && i == 2
+                {
                     continue;
                 }
-                if y_coord == (y_dim - 1) && j == 2 {
+                if working_canvas.boundry_region_list[index].y
+                    == (working_canvas.canvas_constraints.y_size - 1)
+                    && j == 2
+                {
                     continue;
                 }
 
-                let neighbor_x_coord: u32 = x_coord + i - 1;
-                let neighbor_y_coord: u32 = y_coord + j - 1;
+                let neighbor_x_coord: u32 = working_canvas.boundry_region_list[index].x + i - 1;
+                let neighbor_y_coord: u32 = working_canvas.boundry_region_list[index].y + j - 1;
 
                 // skip un-colored
-                let neighbor_color: Rgb<u8> = *canvas.get_pixel(neighbor_x_coord, neighbor_y_coord);
-                if neighbor_color == Rgb([0, 0, 0]) {
+                let neighbor_color: Rgb<u8> = *working_canvas
+                    .image
+                    .get_pixel(neighbor_x_coord, neighbor_y_coord);
+                if neighbor_color == Rgb([0u8, 0u8, 0u8]) {
                     continue;
                 }
 
@@ -178,18 +227,42 @@ fn get_best_position_for_color(
         }
 
         let avg_color_difference = color_difference_sum / neighbor_count as f32;
+
+        // Check for MIN
         if avg_color_difference < min_color_difference {
             min_color_difference = avg_color_difference;
             best_position_index = index;
-        } else if avg_color_difference == min_color_difference && random::<bool>() {
+        }
+        // in case of a tie, choose randomly
+        else if avg_color_difference == min_color_difference && random::<bool>() {
             min_color_difference = avg_color_difference;
             best_position_index = index;
         }
     }
 
-    if boundry_region.len() > 1 {
-        return boundry_region.swap_remove(best_position_index);
+    if working_canvas.boundry_region_list.len() > 1 {
+        working_canvas.boundry_region_image.put_pixel(
+            working_canvas.boundry_region_list[best_position_index].x,
+            working_canvas.boundry_region_list[best_position_index].y,
+            Luma([0u8]),
+        );
+        return Pixel {
+            color: target_color,
+            position: working_canvas
+                .boundry_region_list
+                .swap_remove(best_position_index),
+        };
     } else {
-        return boundry_region.remove(best_position_index);
+        working_canvas.boundry_region_image.put_pixel(
+            working_canvas.boundry_region_list[best_position_index].x,
+            working_canvas.boundry_region_list[best_position_index].y,
+            Luma([0u8]),
+        );
+        return Pixel {
+            color: target_color,
+            position: working_canvas
+                .boundry_region_list
+                .remove(best_position_index),
+        };
     }
 }
